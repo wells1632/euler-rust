cc78e60f8bae24dd9d40752159bbbf66757c5b4f
Jon Akers <jka@inthewings.net>
2025-08-28 21:18:43 +0000

    Adding some commentary

--- e07/src/main.rs
+++ e07/src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
+    // By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can
+    // see that the 6th prime is 13.
+    //
+    // What is the 10,001st prime number?
+
     use std::io::{stdin,stdout,Write};
     let mut s=String::new();
     let mut prime = 2;
