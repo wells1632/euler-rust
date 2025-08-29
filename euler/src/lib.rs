026a4e2f024be0f6fd6038fa0e91589265bed134
Jon Akers <jka@inthewings.net>
2025-08-29 15:41:20 +0000

    Updated to include i32 version of prime detector

--- euler/src/lib.rs
+++ euler/src/lib.rs
@@ -9,6 +9,17 @@ pub fn check_prime_i64(n: i64) -> bool {
     return true;
 }
 
+pub fn check_prime_i32(n: i32) -> bool {
+    let mut i = 2;
+    while i*i<=n {
+        if n%i == 0 {
+            return false;
+        }
+        i+=1;
+    }
+    return true;
+}
+
 pub fn check_palindrome_i32(n: i32) -> bool {
     let s: String = n.to_string();
     let mut r: String = "".to_string();
