3b58ae3398905052c5dff0c5a64f70a237f05fed
Jon Akers <jka@inthewings.net>
2025-09-18 20:42:07 +0000

    Euler 20 Complete

--- e20/src/main.rs
+++ e20/src/main.rs
@@ -1,3 +1,23 @@
+use num_traits::One;
+use num_bigint::BigUint;
+
 fn main() {
-    println!("Hello, world!");
+
+    let result = factorial(100);
+    
+    println!("factorial(100) = {}", result);
+    println!("");
+    let mut sum = 0;
+    for val in result.to_string().chars() {
+        sum += val as i32 - 0x30;
+    }
+    println!("Sum of digits: {}", sum);
+}
+    
+fn factorial(n: usize) -> BigUint {
+    let mut total = BigUint::one();
+    for i in 1..=n {
+        total = total * i;
+    }
+    return total;
 }
